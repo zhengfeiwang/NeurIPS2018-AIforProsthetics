bdb96daadbb1e4437c335b001b4934bd3c5241f5
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2018-07-24 11:40:47 +0800

    reset default configuration

--- baselines/ddpg/local_run.sh
+++ baselines/ddpg/local_run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python train.py --num-workers=24 --num-cpus=24 --reward-shaping
+python train.py --num-workers=24 --num-cpus=28 \
+--actor-hiddens=800-400 --critic-hiddens=800-400 \
+--actor-activation=selu --critic-activation=selu \
+--reward-shaping 
