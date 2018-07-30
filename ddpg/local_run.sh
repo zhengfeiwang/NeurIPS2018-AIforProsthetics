e165332a2c7eef42b652090a71d57e7a27376fdd
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2018-07-30 13:00:20 +0800

    increase noise level

--- ddpg/local_run.sh
+++ ddpg/local_run.sh
@@ -2,5 +2,6 @@
 python train.py --num-workers=48 --num-cpus=48 \
 --actor-activation=selu --critic-activation=selu \
 --actor-learning-rate=3e-4 --critic-learning-rate=3e-4 \
+--noise-level=0.5 \
 --reward-type=shaped --gpu \
 --checkpoint-interval=10 \
