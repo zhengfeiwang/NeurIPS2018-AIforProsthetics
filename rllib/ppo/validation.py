c51ad51e2e73513566ccddb3953e0e1df7afcc7b
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2018-08-10 15:03:04 +0800

    validation.py still need some update

--- rllib/ppo/validation.py
+++ rllib/ppo/validation.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     config = ppo.DEFAULT_CONFIG.copy()
 
     evaluator = Evaluator(
-        args.action_repeat, 
+        args.frameskip, 
         render=True if args.no_render is False else False, 
     )
 
