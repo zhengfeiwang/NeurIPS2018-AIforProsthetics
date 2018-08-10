9fb6e1fe61bae5fe33db6974c0d31b3748f3aeed
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2018-08-10 14:42:45 +0800

    confirm sample batch size

--- rllib/ppo/cluster_run.sh
+++ rllib/ppo/cluster_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 python main.py \
---frameskip=5 --accuracy=5e-5 \
+--frameskip=5 --accuracy=1e-3 \
 --num-workers=80 --num-cpus=80 \
 --cluster \
 --num-gpus=4 \
---sample=2048  --sample-batch=16 \
---reward=standing \
+--sample=1024  --sample-batch=16 \
+--reward=shaped \
 --epochs=10 --hiddens=256-256 --activations=relu \
 --batch-size=256 --learning-rate=5e-5 \
 --seed=60730 \
---iterations=500 --checkpoint-interval=50 --validation-interval=1
+--iterations=100 --checkpoint-interval=10 --validation-interval=1
