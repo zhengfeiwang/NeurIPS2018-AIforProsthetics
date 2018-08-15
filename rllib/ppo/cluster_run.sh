e342f6376711450cc684065949905662053b6e07
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2018-08-15 15:31:32 +0800

    new training settings

--- rllib/ppo/cluster_run.sh
+++ rllib/ppo/cluster_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 python main.py \
 --frameskip=5 --accuracy=1e-3 \
---num-workers=120 --num-cpus=120 \
+--num-workers=60 --num-cpus=60 \
 --cluster \
 --gpu --num-gpus=4 \
---sample=1024  --sample-batch=10 \
---reward=shaped \
---gamma=0.997 \
---epochs=10 --hiddens=256-256 --activations=relu \
---batch-size=256 --learning-rate=1e-5 \
+--sample=1024  --sample-batch=16 \
+--reward=velocity \
+--gamma=0.95 \
+--epochs=30 --hiddens=256-256 --activations=selu \
+--batch-size=256 --learning-rate=5e-5 \
 --seed=60730 \
---iterations=100 --checkpoint-interval=5 --validation-interval=1
+--iterations=500 --checkpoint-interval=5 --validation-interval=1
