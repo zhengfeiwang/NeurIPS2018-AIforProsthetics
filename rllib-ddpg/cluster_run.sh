9e4253871047edb194e7b4f28382d9d9447825f0
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2018-08-15 14:38:02 +0800

    velocity reward

--- rllib-ddpg/cluster_run.sh
+++ rllib-ddpg/cluster_run.sh
@@ -5,7 +5,7 @@ python main.py \
 --cluster --gpu \
 --sample=1024 --sample-batch=16 \
 --warmup=1000 \
---reward=shaped \
+--reward=velocity \
 --gamma=0.95 \
 --batch-size=128 --actor-lr=3e-4 --critic-lr=3e-3 \
 --seed=60730 \
