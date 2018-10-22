d4b1051b331506fc9d312ad99d04e4f3dcab8b71
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2018-10-22 11:47:13 +0800

    new course_1 for training start
    basic reward of walk and lean_back penalty

--- round2/round2_env.py
+++ round2/round2_env.py
@@ -110,6 +110,9 @@ class Round2Env(ProstheticsEnv):
         pelvis_vx = state_desc["body_vel"]["pelvis"][0]
         reward = pelvis_vx * 4 + 2
 
+        lean_back = max(0, state_desc["body_pos"]["pelvis"][0] - state_desc["body_pos"]["head"][0] - 0.2)
+        reward -= lean_back * 40
+
         return reward * 0.05
 
 
