d27e5be96b1ab7b774c75eaa611f8bb515fdaed7
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2018-10-29 02:10:09 +0800

    survival reward
    add survival reward and scale the reward_round2

--- round2/round2_env.py
+++ round2/round2_env.py
@@ -98,7 +98,8 @@ class Round2Env(ProstheticsEnv):
         if not prev_state_desc:
             return 0
 
-        reward = super().reward_round2()
+        reward_round2 = super().reward_round2()
+        reward = reward_round2 * 0.5 + 1
 
         lean_back = max(0, state_desc["body_pos"]["pelvis"][0] - state_desc["body_pos"]["head"][0] - 0.2)
         reward -= lean_back * 40
