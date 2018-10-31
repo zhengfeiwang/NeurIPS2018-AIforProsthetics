0324544a76e794aebb7e867bda40b8ca9e9264b7
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2018-10-31 21:09:21 +0800

    restart due to action clip

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 python -m nips.round2_train \
     --num-timesteps=1000000 \
-    --num-steps=256 \
+    --num-steps=128 \
     --num-minibatches=16 \
     --num-cpus=20 --num-casks=4 \
     --num-gpus=4 \
     --save-interval=5 \
-    --seed=30 --repeat=1 \
-    --checkpoint-path=checkpoints/origin/00000
+    --seed=30 --repeat=2
